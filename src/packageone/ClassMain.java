82d30a203596bae32e6e8dbcf2c04b22183abf6e
Satish <Satish@Satish-PC>
2020-03-20 08:12:26 +0530

    This is to add comments in main method

--- src/packageone/ClassMain.java
+++ src/packageone/ClassMain.java
@@ -13,7 +13,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.BeforeTest;
 
 public class ClassMain extends Course_Schedule{
-	
+	//This is a comment at ClassMain Method
 	@BeforeTest
 	public void MethodMain() throws IOException, InterruptedException {
 		int Column_Count=0;
